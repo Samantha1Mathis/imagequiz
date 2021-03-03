2a927dcad0c4a5bdc6482fd3adb807949fddc7c4
Samantha Mathis <Samantha122@email.arizona.edu>
2021-03-03 10:08:36 -0700

    Create React App

---
+++ imagequiz/src/components/Home.js
@@ -0,0 +1,15 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+function Home(){
+    return (
+        <Row>
+            <col>
+                My Home Page
+            </col>
+        </Row>
+    );
+}
+
+export default Home;
